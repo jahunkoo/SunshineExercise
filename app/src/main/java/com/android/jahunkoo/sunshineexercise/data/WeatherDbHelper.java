5f18f32ad0ca463203a847ec7814a7bb399c600a
jahunkoo <koo10682@gmail.com>
2015-01-16 16:37:34 +0900

    jUnit test fail

--- app/src/main/java/com/android/jahunkoo/sunshineexercise/data/WeatherDbHelper.java
+++ app/src/main/java/com/android/jahunkoo/sunshineexercise/data/WeatherDbHelper.java
@@ -26,12 +26,11 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
         final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
                 LocationEntry._ID + " INTEGER PRIMARY KEY," +
                 LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
-                LocationEntry.COLUMN_CITY_NAME + "TEXT NOT NULL," +
+                LocationEntry.COLUMN_CITY_NAME + "TEXT NOT NULL, " +
                 LocationEntry.COLUMN_COORD_LAT + "REAL NOT NULL, " +
                 LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL, " +
                 "UNIQUE (" + LocationEntry.COLUMN_LOCATION_SETTING + ") ON CONFLICT IGNORE"+
                 " );";
-
 
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
                 WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
