4c60f4c91d2960e4ce996e11221f298c2e920e08
jahunkoo <koo10682@gmail.com>
2015-01-16 22:42:50 +0900

    doing TestProvider

---
+++ app/src/androidTest/java/com/android/jahunkoo/sunshineexercise/TestProvider.java
@@ -0,0 +1,7 @@
+package com.android.jahunkoo.sunshineexercise;
+
+/**
+ * Created by Jahun Koo on 2015-01-16.
+ */
+public class TestProvider {
+}
---
+++ app/src/main/java/com/android/jahunkoo/sunshineexercise/data/WeatherProvider.java
@@ -0,0 +1,7 @@
+package com.android.jahunkoo.sunshineexercise.data;
+
+/**
+ * Created by Jahun Koo on 2015-01-16.
+ */
+public class WeatherProvider {
+}
